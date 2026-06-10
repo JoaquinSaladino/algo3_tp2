bed0abcfec486fb95dd072a3f4550e84c29661f7
lucasvasquez0711 <cvasquez@fi.uba.ar>
2026-06-10 19:55:10 -0300

    Agregar imports borrados

--- src/main/java/edu/fiuba/algo3/modelo/Habilidades/Investigar.java
+++ src/main/java/edu/fiuba/algo3/modelo/Habilidades/Investigar.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.modelo.Habilidades;
 
+import edu.fiuba.algo3.modelo.AccionNocturna.AInvestigar;
+import edu.fiuba.algo3.modelo.AccionNocturna.AccionNocturna;
 import edu.fiuba.algo3.modelo.Jugador;
 
 public class Investigar implements HabilidadNocturna {
@@ -9,4 +11,5 @@ public class Investigar implements HabilidadNocturna {
     public AccionNocturna ejecutar(Jugador autor, Jugador objetivo) {
         return new AInvestigar(autor,objetivo);
     }
+
 }
