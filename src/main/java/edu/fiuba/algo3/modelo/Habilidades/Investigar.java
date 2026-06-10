77537381a8b700cce15e987c5eec3bd6c6a19b9b
Reiji <nsuyama@fi.uba.ar>
2026-06-10 12:10:12 -0300

    se agrega logica a Investigar

--- src/main/java/edu/fiuba/algo3/modelo/Habilidades/Investigar.java
+++ src/main/java/edu/fiuba/algo3/modelo/Habilidades/Investigar.java
@@ -3,9 +3,13 @@ package edu.fiuba.algo3.modelo.Habilidades;
 import edu.fiuba.algo3.modelo.Jugador;
 
 public class Investigar implements HabilidadNocturna {
+    private boolean resultado;
 
     @Override
     public void ejecutar(Jugador objetivo) {
-
+        this.resultado = objetivo.esMafia();
+    }
+    public boolean getResultado() {
+        return resultado;
     }
 }
