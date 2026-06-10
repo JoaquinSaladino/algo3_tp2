b15835c7c7418f132fd6d2890b8ca09e9b89ebcc
Joaquin Saladino <saladinojoaquin@gmail.com>
2026-06-10 20:30:17 -0300

    Avances varios

--- src/main/java/edu/fiuba/algo3/modelo/Habilidades/Investigar.java
+++ src/main/java/edu/fiuba/algo3/modelo/Habilidades/Investigar.java
@@ -2,13 +2,17 @@ package edu.fiuba.algo3.modelo.Habilidades;
 
 import edu.fiuba.algo3.modelo.AccionNocturna.AInvestigar;
 import edu.fiuba.algo3.modelo.AccionNocturna.AccionNocturna;
+import edu.fiuba.algo3.modelo.Excepciones.ObjetivoInvalidoException;
 import edu.fiuba.algo3.modelo.Jugador;
 
 public class Investigar implements HabilidadNocturna {
-    private boolean resultado;
+    private Jugador objetivoAnterior;
 
     @Override
     public AccionNocturna ejecutar(Jugador autor, Jugador objetivo) {
+        if (objetivo == objetivoAnterior)
+            throw new ObjetivoInvalidoException();
+        objetivoAnterior = objetivo;
         return new AInvestigar(autor,objetivo);
     }
 
--- src/main/java/edu/fiuba/algo3/modelo/Habilidades/Proteger.java
+++ src/main/java/edu/fiuba/algo3/modelo/Habilidades/Proteger.java
@@ -2,12 +2,16 @@ package edu.fiuba.algo3.modelo.Habilidades;
 
 import edu.fiuba.algo3.modelo.AccionNocturna.AProteger;
 import edu.fiuba.algo3.modelo.AccionNocturna.AccionNocturna;
+import edu.fiuba.algo3.modelo.Excepciones.ObjetivoInvalidoException;
 import edu.fiuba.algo3.modelo.Jugador;
 
 public class Proteger implements HabilidadNocturna {
-
+    private Jugador objetivoAnterior;
     @Override
     public AccionNocturna ejecutar(Jugador autor, Jugador objetivo) {
+        if (objetivoAnterior == objetivo)
+            throw new ObjetivoInvalidoException();
+        objetivoAnterior = objetivo;
         return new AProteger(autor,objetivo);
     }
 }
